b99bb963ac766080c37b59c02f7f0f9c56cc079c
lauren.dunn <lauren.dunn@ukhsa.gov.uk>
2023-09-07 13:58:13 +0100

    added extra features to streamlit dashboard so user can choose CPIH or CPI, also fixed date range filter

--- notebooks/app.py
+++ notebooks/app.py
@@ -3,80 +3,84 @@ from typing import List, Tuple
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import datetime
 
 def set_page_config():
     st.set_page_config(
-        page_title="Consumer Price Index by division levels",
+        page_title="The Consumer Prices Index including owner occupiers' housing costs by division levels",
         page_icon=":random:",
         layout="wide",
         initial_sidebar_state="expanded",
     )
     st.markdown("<style> footer {visibility: hidden;} </style>", unsafe_allow_html=True)
-    
+
 
 @st.cache_data
 def load_data() -> pd.DataFrame:
-    data = pd.read_csv("../data/processed/merged_df_long.csv")
+    data = pd.read_csv("../data/processed/CPIH_and_CPI_df.csv")
     data['Date'] = pd.to_datetime(data['Date'])
     return data
+
 
 def filter_data(data: pd.DataFrame, column: str, values: List[str]) -> pd.DataFrame:
     return data[data[column].isin(values)] if values else data
 
-def display_sidebar(data: pd.DataFrame) -> Tuple[List[str], List[str], List[str]]:
+def display_sidebar(data: pd.DataFrame) -> Tuple[List[str], List[str], List[str], Tuple[datetime.date, datetime.date]]:
     st.sidebar.header("Filters")
-    start_date = pd.Timestamp(st.sidebar.date_input("Start date", data['Date'].min().date()))
-    end_date = pd.Timestamp(st.sidebar.date_input("End date", data['Date'].max().date()))
+    
+    # Get the minimum and maximum dates from the dataset
+    min_start_date = data['Date'].min().date()
+    max_end_date = data['Date'].max().date()
 
+    start_date = pd.Timestamp(st.sidebar.date_input("Start date", min_start_date, min_value=min_start_date, max_value=max_end_date))
+    end_date = pd.Timestamp(st.sidebar.date_input("End date", max_end_date, min_value=min_start_date, max_value=max_end_date))
+    
+    
+    CPI_divisions = sorted(data['Index Name'].unique())
+    selected_cpi_divisions = st.sidebar.multiselect("CPIH/CPI Divisions", CPI_divisions)
 
-    CPI_divisions = sorted(data['variable'].unique())
-    selected_cpi_divisions = st.sidebar.multiselect("CPI Divisions", CPI_divisions)
-
-
-    #product_lines = sorted(data['PRODUCTLINE'].unique())
-    #selected_product_lines = st.sidebar.multiselect("Product lines", product_lines, product_lines)
-
-    #selected_countries = st.sidebar.multiselect("Select Countries", data['COUNTRY'].unique())
-
-    #selected_statuses = st.sidebar.multiselect("Select Order Statuses", data['STATUS'].unique())
-
-    return selected_cpi_divisions # selected_product_lines, selected_countries, selected_statuses
+    return selected_cpi_divisions, (start_date, end_date)
 
 
 def display_charts(data: pd.DataFrame):
-    combine_product_lines = st.checkbox("Combine CPI Division Lines", value=True)
+    CPIH_dataset = st.checkbox("CPIH Dataset", value=True)
+    CPI_dataset = st.checkbox("CPI Dataset", value=True)
+    combine_CPIH_lines = st.checkbox("Combine CPIH Division Lines", value=True)
+    combine_CPI_lines = st.checkbox("Combine CPI Division Lines", value=True)
+    CPIH_CPI_separate = st.checkbox("Separate lines for CPIH and CPI", value=True)
 
-    if combine_product_lines:
-        fig = px.area(data, x='Date', y='value',
-                      title="Customer Price Index Over Time", width=900, height=500)
-    else:
-        fig = px.area(data, x='Date', y='value', color='variable',
-                      title="Customer Price Index Over Time", width=900, height=500)
+    
+    if CPIH_CPI_separate:
+        grouped_df = data.groupby(['Date','Name'])['Value'].mean().reset_index()
+        fig = px.line(grouped_df, x='Date', y='Value', color='Name',
+              title="Customer Price Index Over Time", width=900, height=500)
+        
+    elif CPIH_dataset:
+        CPIH_data = data[data['Name'] == 'CPIH'].copy()
+        if combine_CPIH_lines:
+            grouped_data = CPIH_data.groupby('Date')['Value'].mean().reset_index()
+            fig = px.area(grouped_data, x='Date', y='Value',
+                        title="Customer Price Index Over Time", width=900, height=500)
+        else:
+            fig = px.area(CPIH_data, x='Date', y='Value', color='Index Name',
+                        title="Customer Price Index Over Time", width=900, height=500)
+            
+    elif CPI_dataset:
+        CPI_data = data[data['Name'] == 'CPI'].copy()
+        if combine_CPI_lines:
+            grouped_data_CPI = CPI_data.groupby('Date')['Value'].mean().reset_index()
+            fig = px.area(grouped_data_CPI, x='Date', y='Value',
+                        title="Customer Price Index Over Time", width=900, height=500)
+        else:
+            fig = px.area(CPI_data, x='Date', y='Value', color='Index Name',
+                        title="Customer Price Index Over Time", width=900, height=500)
+        
 
     fig.update_layout(margin=dict(l=20, r=20, t=50, b=20))
     fig.update_xaxes(rangemode='tozero', showgrid=False)
     fig.update_yaxes(rangemode='tozero', showgrid=True)
     st.plotly_chart(fig, use_container_width=True)
 
-    #col1, col2, col3 = st.columns(3)
-
-    #with col1:
-    #    st.subheader("Top 10 Divisions")
-    #    top_customers = data.groupby('CUSTOMERNAME')['SALES'].sum().reset_index().sort_values('SALES',
-    #                                                                                          ascending=False).head(10)
-    #    st.write(top_customers)
-
-    #with col2:
-    #    st.subheader("Top 10 Products by Sales")
-    #    top_products = data.groupby(['PRODUCTCODE', 'PRODUCTLINE'])['SALES'].sum().reset_index().sort_values('SALES',
-    #                                                                                                         ascending=False).head(
-    #        10)
-    #    st.write(top_products)
-
-    #with col3:
-    #    st.subheader("Total Sales by Product Line")
-    #    total_sales_by_product_line = data.groupby('PRODUCTLINE')['SALES'].sum().reset_index()
-    #    st.write(total_sales_by_product_line)
 
 
 def main():
@@ -84,18 +88,20 @@ def main():
 
     data = load_data()
 
-    st.title("Consumer Price Index by division levels")
+    st.title("The Consumer Prices Index including owner occupiers' housing costs by division levels")
 
-    selected_cpi_divisions = display_sidebar(data)
+    st.markdown("The Consumer Price Index measures the overall change in consumer prices based on a representative basket of goods and services over time.")
+
+    selected_cpi_divisions, date_range = display_sidebar(data)
 
     filtered_data = data.copy()
-    filtered_data = filter_data(filtered_data, "variable", selected_cpi_divisions)
-    #filtered_data = filter_data(filtered_data, 'COUNTRY', selected_countries)
-    #filtered_data = filter_data(filtered_data, 'STATUS', selected_statuses)
+    filtered_data = filter_data(filtered_data, "Index Name", selected_cpi_divisions)
+    
+    # Unpack the date range tuple
+    start_date, end_date = date_range
 
-    #kpis = calculate_kpis(filtered_data)
-    #kpi_names = ["Total Sales", "Total Orders", "Average Sales per Order", "Unique Customers"]
-    #display_kpi_metrics(kpis, kpi_names)
+    # Apply date filtering here
+    filtered_data = filtered_data[(filtered_data['Date'] >= start_date) & (filtered_data['Date'] <= end_date)]
 
     display_charts(filtered_data)
 
